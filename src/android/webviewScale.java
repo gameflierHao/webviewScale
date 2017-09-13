56ce6651d2b32197fb0dae8759ed88cc297fa3fe
Howard <zhanghaowu@gmail.com>
2017-09-13 15:56:10 +0800

    v2

--- src/android/webviewScale.java
+++ src/android/webviewScale.java
@@ -2,17 +2,14 @@ package com.tagfans.plugin.webviewScale;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Activity;
 
 import org.apache.cordova.*;
 
-import java.io.IOException;
 
 
 
-public class MainActivity extends CordovaActivity
+public class webviewScale extends CordovaActivity
 {
    @Override
    public void onCreate(Bundle savedInstanceState)
