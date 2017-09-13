3467070b87acc355a6cff18af0065824bfcb1e13
Howard <zhanghaowu@gmail.com>
2017-09-13 16:00:51 +0800

    v3

--- src/android/webviewScale.java
+++ src/android/webviewScale.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
 
+import java.lang.Object
 import org.apache.cordova.*;
 
 
@@ -15,16 +16,10 @@ public class webviewScale extends CordovaActivity
    public void onCreate(Bundle savedInstanceState)
   {
    super.onCreate(savedInstanceState);
-  if (appView == null) {
-  init();
-   }
- 
-   // If keepRunning
-   this.keepRunning = preferences.getBoolean("KeepRunning", true);
 
    ((WebView) appView.getView()).getSettings().setUseWideViewPort(true);
    ((WebView) appView.getView()).setInitialScale(55);
-   appView.loadUrlIntoView(launchUrl, true);
+    appView.loadUrlIntoView(launchUrl, true);
    }
 }
 
